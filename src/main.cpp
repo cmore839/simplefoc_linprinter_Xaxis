c2499b9f13ed18b90ff5e588ee6de60f38c12174
cmore839 <85714349+cmore839@users.noreply.github.com>
2024-12-17 22:07:21 +1100

    Low side working individual motors

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,13 @@
 //Motor & Encoder Setup
 BLDCMotor M1 = BLDCMotor(2); //Probably leave as 2 for every linear motor combo
 BLDCMotor M2 = BLDCMotor(2);
-BLDCDriver3PWM DR1 = BLDCDriver3PWM(PB6, PB4, PB10, PA9); //M1 - Lower
-BLDCDriver3PWM DR2 = BLDCDriver3PWM(PC7, PB3, PA5, PA8); //M2 - Upper
-Encoder E1 = Encoder(PC6, PC8, 1110); //M1 - Lower, X1=1108-1109 9/12/24
+BLDCDriver3PWM DR1 = BLDCDriver3PWM(PC9, PB4, PC7, PA9); //M1 - Lower //PB6, PB4, PB10, PA9
+//ORIG - D10(PB6)=TM4-1, D5(PB4)=TIM3-1, D6(PB10)=TIM2-3
+//NEW - D31(PC9)=TM3-4, D5(PB4)=TIM3-1, D9(PC7)=TIM3-2
+BLDCDriver3PWM DR2 = BLDCDriver3PWM(PB10, PB3, PA5, PA8); //M2 - Upper //PC7, PB3, PA5, PA8
+//ORIG - D9(PC7)=TIM3-2, D3(PB3)=TIM2-2, D13(PA5)=TIM2-1
+//NEW - D6(PB10)=TIM2-3, D3(PB3)=TIM2-2, D13(PA5)=TIM2-1
+Encoder E1 = Encoder(PC6, PC8, 1110); //M1 - Lower, X1=1108-1109 9/12/24 //PC6, PC8, 1110
 Encoder E2 = Encoder(PB1, PB2, 1110); //M2 - Upper, X2=1110, 9/12/24
 void doA1(){E1.handleA();}
 void doB1(){E1.handleB();}
@@ -30,17 +34,20 @@ unsigned long looptime;
 int loopcounter = 0;
 int loopiter = 10;
 
-float voltage_set = 12;
-float M_angle_P = 50;
-float M_velocity_P = 0.5;
+float voltage_set = 24;
+float velocity_set = 40;
+float M_angle_P = 50; //250
+float M_velocity_P = 1; //10
 
 //Inline sense and Step/Dir
-InlineCurrentSense CS1  = InlineCurrentSense(0.01, 50, A0, A2, _NC);
-InlineCurrentSense CS2  = InlineCurrentSense(0.01, 50, A1, A3, _NC);
+LowsideCurrentSense CS1  = LowsideCurrentSense(0.01, 50, PA_0_ALT1, A2, _NC); //ADC2 - PA_0_ALT1, A2
+LowsideCurrentSense CS2  = LowsideCurrentSense(0.01, 50, A1, A3, _NC); //ADC1 - A1, A3
 StepDirListener SD1 = StepDirListener(PA15, PC12, 0.0014);
 void onStep() { SD1.handle(); } 
 
 void setup() {
+  Serial.begin(115200); // to output the debug information to the serial
+  SimpleFOCDebug::enable(&Serial);
   SimpleFOC_CORDIC_Config();
 
   //Motor 1
@@ -50,10 +57,10 @@ void setup() {
   E2.quadrature = Quadrature::ON;
   E2.init();
   E2.enableInterrupts(doA2, doB2);
-  M1.linkSensor(&E1);
 
+  M1.linkSensor(&E1);
   // driver config
-  DR1.pwm_frequency = 50000;
+  DR1.pwm_frequency = 25000;
   DR1.voltage_power_supply = voltage_set;
   DR1.init();
   CS1.linkDriver(&DR1);
@@ -61,43 +68,45 @@ void setup() {
   M1.init();
   CS1.init();
   M1.linkCurrentSense(&CS1);
-  M1.voltage_sensor_align = 12;
+  M1.voltage_sensor_align = 8;
   M1.foc_modulation = FOCModulationType::SpaceVectorPWM;
   M1.controller = MotionControlType::angle;
   M1.torque_controller = TorqueControlType::foc_current;
 
   // setting the limits
-  M1.velocity_limit = 20;
+  M1.velocity_limit = velocity_set;
   M1.voltage_limit = voltage_set;
   M1.current_limit = 1;
 
   // velocity PID controller parameters
-  M1.PID_velocity.P = M_velocity_P; //0.6
+  M1.PID_velocity.P = M_velocity_P;
   M1.PID_velocity.I = 0;
   M1.PID_velocity.D = 0;
   M1.PID_velocity.output_ramp = 0;
   M1.LPF_velocity.Tf = 0.001;
    
   // angle PID controller 
-  M1.P_angle.P = M_angle_P; //100 
-  M1.P_angle.I = 0; //0.4
-  M1.P_angle.D = 0; //2.0
+  M1.P_angle.P = M_angle_P;
+  M1.P_angle.I = 0;
+  M1.P_angle.D = 0;
   M1.P_angle.output_ramp = 0;
   M1.LPF_angle.Tf = 0;
 
   // foc current control parameters
   M1.PID_current_q.P = 1;
   M1.PID_current_q.I= 0.1;
+  M1.PID_current_q.limit = voltage_set; 
   M1.PID_current_d.P= 1;
   M1.PID_current_d.I = 0.1;
+  M1.PID_current_d.limit = voltage_set; 
   M1.LPF_current_q.Tf = 0.01; 
   M1.LPF_current_d.Tf = 0.01; 
-  
+
   //Motor 2
   M2.linkSensor(&E2);
 
   // driver config
-  DR2.pwm_frequency = 50000;
+  DR2.pwm_frequency = 25000;
   DR2.voltage_power_supply = voltage_set;
   DR2.init();
   CS2.linkDriver(&DR2);
@@ -105,13 +114,13 @@ void setup() {
   M2.init();
   CS2.init();
   M2.linkCurrentSense(&CS2);
-  M2.voltage_sensor_align = 12;
+  M2.voltage_sensor_align = 8;
   M2.foc_modulation = FOCModulationType::SpaceVectorPWM;
   M2.controller = MotionControlType::angle;
   M2.torque_controller = TorqueControlType::foc_current;
 
   // setting the limits
-  M2.velocity_limit = 20;
+  M2.velocity_limit = velocity_set;
   M2.voltage_limit = voltage_set;
   M2.current_limit = 1;
 
@@ -136,8 +145,9 @@ void setup() {
   M2.PID_current_d.I = 0.1;
   M2.LPF_current_q.Tf = 0.01; 
   M2.LPF_current_d.Tf = 0.01; 
-
+  
   M1.initFOC(); //skip for open loop
+  delay(1000);
   M2.initFOC(); //skip for open loop
 
   // 655us with SD
