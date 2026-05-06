3d580a96700c8d711557cd6c88adc56a035d22ef
cmore839 <85714349+cmore839@users.noreply.github.com>
2026-05-06 20:13:25 +1000

    Debounce - Working!

--- src/main.cpp
+++ src/main.cpp
@@ -53,11 +53,11 @@ static void MX_TIM1_Init(void)
   sConfig.IC1Polarity = TIM_ICPOLARITY_RISING;
   sConfig.IC1Selection = TIM_ICSELECTION_DIRECTTI;
   sConfig.IC1Prescaler = TIM_ICPSC_DIV1;
-  sConfig.IC1Filter = 0;
+  sConfig.IC1Filter = 4;
   sConfig.IC2Polarity = TIM_ICPOLARITY_RISING;
   sConfig.IC2Selection = TIM_ICSELECTION_DIRECTTI;
   sConfig.IC2Prescaler = TIM_ICPSC_DIV1;
-  sConfig.IC2Filter = 0;
+  sConfig.IC2Filter = 4;
   if (HAL_TIM_Encoder_Init(&htim1, &sConfig) != HAL_OK)
   {
     Error_Handler();
@@ -140,8 +140,8 @@ LowsideCurrentSense CS2  = LowsideCurrentSense(0.01, 50, A3, A1, _NC);
 // Hardware Listener Object (Linked to TIM1)
 HWStepDirListener SD1 = HWStepDirListener(&htim1, 0.0014);
 
-// Low pass filter for the feedforward velocity (5ms time constant)
-LowPassFilter v_ff_filter = LowPassFilter(0.005);
+// Low pass filter for the feedforward velocity (2ms time constant)
+LowPassFilter v_ff_filter = LowPassFilter(0.002);
 
 void startup(){
   while (startupcount < 3000000){
